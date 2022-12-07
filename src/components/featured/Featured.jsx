6dfe8cf46328a67e403c979e50df4ee48440d7fc
Mohammad Ghasempour <m.gh48nor@gmail.com>
2022-12-07 10:52:53 +0100

    chart in the right side of home page created

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,6 +2,8 @@ import "./featured.scss";
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 
 const Featured = () => {
   const percentage = 70;
@@ -19,6 +21,29 @@ const Featured = () => {
         <p className="title">Total sales made today</p>
         <p className="amount">$530</p>
         <p className="description">Previous transaction processing. Last payment may not be included.</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+            <div className="itemResult negative">
+              <ExpandMoreOutlinedIcon fontSize="small"/>
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <div className="resultAmount">$12.4K</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
