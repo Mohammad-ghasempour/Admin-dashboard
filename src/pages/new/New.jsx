4ae7473c06763abd14d6e3e3e82c875a204b678d
Mohammad Ghasempour <m.gh48nor@gmail.com>
2022-12-14 11:40:48 +0100

    during building form for new page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,52 @@
-import "./new.scss"
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./new.scss";
 
 const New = () => {
   return (
-    <div>New</div>
-  )
-}
+    <div className="new">
+      <Sidebar />
 
-export default New
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add new User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt="No image"
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="Mohammad-ghasempour" />
+              </div>
+              <div className="formInput">
+                <label>Name and Surname</label>
+                <input type="text" placeholder="Mohammad Ghasempour" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="M.gh84nor" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+47 938 28 281" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="+47 938 28 281" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
