21e12c4eec780622c1f7bc84ef1e211fcf24e757
Mohammad Ghasempour <m.gh48nor@gmail.com>
2022-12-15 10:09:23 +0100

    create URL for image uploaded is done

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./new.scss";
-import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import { useState } from "react";
 
-const New = ({inputs , title}) => {
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+  console.log(file);
   return (
     <div className="new">
       <Sidebar />
@@ -16,24 +19,35 @@ const New = ({inputs , title}) => {
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              alt="No image"
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt="image"
             />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
-                <label htmlFor="uploadImage">Image: <DriveFolderUploadIcon className="icon"/></label>
-                <input type="file" id="uploadImage" style={{display:"none"}} />
+                <label htmlFor="uploadImage">
+                  Image: <DriveFolderUploadIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="uploadImage"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
-             {inputs.map((input)=>{
-                return(
-                  <div className="formInput">
-                  <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder}/>
-                </div>
-                )
-             })}
+              {inputs.map((input) => {
+                return (
+                  <div className="formInput" key={input.id}>
+                    <label>{input.label}</label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                );
+              })}
               <button>Send</button>
             </form>
           </div>
