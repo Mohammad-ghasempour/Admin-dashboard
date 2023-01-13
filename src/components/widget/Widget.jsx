34db8183e284a6fd9f1c666059f514f61e97656b
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-13 13:36:55 +0100

    USERS badget improved

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -4,10 +4,16 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import { useEffect, useState } from "react";
+import { Collections } from "@mui/icons-material";
+import { db } from "../../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 const Widget = ({ type }) => {
+  const [usersCount, setUsersCount] = useState(null);
+  const [diff, setDiff] = useState(null);
   //sample data
-  const amount = 100;
+
   let data;
   switch (type) {
     case "users":
@@ -28,7 +34,15 @@ const Widget = ({ type }) => {
         title: "ORDERS",
         isMoney: false,
         link: "see all orders",
-        icon: <ShoppingCartOutlinedIcon className="icon"  style={{ color: "goldenrod", backgroundColor: "rgba(218,165,32,0.2)" }} />,
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
       };
       break;
     case "earnings":
@@ -36,7 +50,12 @@ const Widget = ({ type }) => {
         title: "EARNING",
         isMoney: true,
         link: "view net earning",
-        icon: <MonetizationOnOutlinedIcon className="icon"  style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }} />,
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
       };
       break;
     case "myBalance":
@@ -44,27 +63,66 @@ const Widget = ({ type }) => {
         title: "BALANCE",
         isMoney: true,
         link: "see details",
-        icon: <AccountBalanceWalletOutlinedIcon className="icon"  style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }} />,
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
       };
       break;
 
     default:
       break;
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const today = new Date();
+      const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+      const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+
+      const lastMonthData = query(
+        collection(db, "users"),
+        where("timeStamp", "<=", today),
+        where("timeStamp", ">", lastMonth)
+      );
+      const prevMonthData = query(
+        collection(db, "users"),
+        where("timeStamp", "<=", lastMonth),
+        where("timeStamp", ">", prevMonth)
+      );
+
+      const querySnapshotLastMonth = await getDocs(lastMonthData);
+      const lastMonthUsersCount = querySnapshotLastMonth.docs.length;
+
+      const querySnapshotPrevMonth = await getDocs(prevMonthData);
+      const prevMonthUsersCount = querySnapshotPrevMonth.docs.length;
+
+      setUsersCount(lastMonthUsersCount);
+      setDiff(
+        ((lastMonthUsersCount - prevMonthUsersCount) / prevMonthUsersCount) *
+          100
+      );
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="widget">
       <div className="left">
         <span className="title">{data.title}</span>
         <span className="counter">
           {data.isMoney && "$"}
-          {amount}
+          {usersCount}
         </span>
         <span className="link">{data.link}</span>
       </div>
       <div className="right">
         <div className="percentage positive">
           <ExpandLessOutlinedIcon />
-          20%
+          {diff}%
         </div>
         {data.icon}
       </div>
